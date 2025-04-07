9e21c27ae4f4054500de6c4f8e7193e822129b35
Afzal <149293919+afzalkhanofficial@users.noreply.github.com>
2025-04-07 12:14:19 +0530

    Importing Libraries & Tools

--- ProcSight.py
+++ ProcSight.py
@@ -1,1 +1,13 @@
-
+import sys
+import psutil
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
+    QLabel, QLineEdit, QTableView, QHeaderView, QMenu, QMessageBox,
+    QListWidget, QListWidgetItem, QStackedWidget, QAbstractItemView,
+    QGraphicsDropShadowEffect, QTabWidget
+)
+from PyQt5.QtCore import (
+    QTimer, Qt, QSortFilterProxyModel, QAbstractTableModel, QModelIndex
+)
+from PyQt5.QtGui import QFont, QColor, QPalette, QIcon
+import pyqtgraph as pg
