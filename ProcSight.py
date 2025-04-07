ba40ac89e442dcc442bea2121b4a8056b92792d7
Afzal <149293919+afzalkhanofficial@users.noreply.github.com>
2025-04-07 12:15:15 +0530

    Custom Process Table Model

--- ProcSight.py
+++ ProcSight.py
@@ -11,3 +11,41 @@ from PyQt5.QtCore import (
 )
 from PyQt5.QtGui import QFont, QColor, QPalette, QIcon
 import pyqtgraph as pg
+
+##############################################################################
+# 1. Custom Process Table Model
+##############################################################################
+class ProcessTableModel(QAbstractTableModel):
+    def __init__(self, processes=None):
+        super().__init__()
+        self.header = ["PID", "Name", "CPU %", "Memory %"]
+        self.processes = processes or []
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        row, col = index.row(), index.column()
+        if role == Qt.DisplayRole:
+            return self.processes[row][col]
+        elif role == Qt.TextAlignmentRole:
+            return Qt.AlignCenter
+        return None
+
+    def rowCount(self, parent=None):
+        return len(self.processes)
+
+    def columnCount(self, parent=None):
+        return len(self.header)
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self.header[section]
+            else:
+                return section
+        return None
+
+    def updateProcesses(self, processes):
+        self.beginResetModel()
+        self.processes = processes
+        self.endResetModel()
